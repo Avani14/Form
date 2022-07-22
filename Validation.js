fa0a40a5f24f6c7857755eb7c6041b301d11e857
Avani14 <trivediavani14@gmail.com>
2022-07-22 19:00:12 +0530

    Added the ability to validate email.

--- Validation.js
+++ Validation.js
@@ -1,8 +1,17 @@
-function validations() {
-    //UC1
-    var name = document.forms["form"]["first-name"].value
+const form = document.getElementById("form")
+const fname = document.getElementById("first-name")
+const email = document.getElementById("email")
+const validations= () => {
+    const name = fname.value.trim()
+    const emailId = email.value.trim()
     const regex = RegExp("^[A-Z]{1}[a-z]*$")
     if (name == "" || !regex.test(name)) {
-        window.alert("Please enter your name properly.");
+        window.alert("Please enter valid name")    
     }
+
+    const emailRegex = RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")
+    if (email == "" || !emailRegex.test(email)) {
+        window.alert("Please enter valid email")  
+    }
+   
 }
