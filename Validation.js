6be40410fe479161ff32e0bdf35e638e6e4539c0
Avani14 <trivediavani14@gmail.com>
2022-07-22 19:13:59 +0530

    Added the ability to validate Phone number.

--- Validation.js
+++ Validation.js
@@ -1,17 +1,22 @@
 const form = document.getElementById("form")
 const fname = document.getElementById("first-name")
 const email = document.getElementById("email")
+const phoneNumber = document.getElementById("phone_number")
 const validations= () => {
     const name = fname.value.trim()
     const emailId = email.value.trim()
+    const phone = phoneNumber.value.trim()
     const regex = RegExp("^[A-Z]{1}[a-z]*$")
     if (name == "" || !regex.test(name)) {
         window.alert("Please enter valid name")    
     }
 
-    const emailRegex = RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")
-    if (email == "" || !emailRegex.test(email)) {
+    const emailRegex = RegExp("^[A-Za-z0-9]*+@[A-Za-z0-9]*+.[A-Za-z]")
+    if (email == "" || emailRegex.test(email) == false) {
         window.alert("Please enter valid email")  
     }
-   
+    const phoneRegex = RegExp("^[91]{1}[7-9]{1}[0-9]{9}$")
+    if (phoneNumber == "" || !phoneRegex.test(phoneNumber) == false) {
+        window.alert("Please enter valid phone number")  
+    }
 }
